13494606da4718dffc8741f4228e23d315d2f3ac
shuftipro <34159299+shuftipro@users.noreply.github.com>
2022-08-25 13:05:37 +0500

    updated sdk with 3.2.0

--- ShuftiPro.podspec
+++ ShuftiPro.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
 #
 
 spec.name         = "ShuftiPro"
-spec.version      = "3.1.0"
+spec.version      = "3.2.0"
 spec.summary      = "Shufti Pro is designed to verify documents in realtime."
 
 # This description is used to generate tags and improve search results.
--- ShuftiPro.xcframework/ios-arm64/ShuftiPro.framework/Headers/ShuftiPro-Swift.h
+++ ShuftiPro.xcframework/ios-arm64/ShuftiPro.framework/Headers/ShuftiPro-Swift.h
@@ -257,55 +257,12 @@ SWIFT_CLASS("_TtC9ShuftiPro7CallApi")
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
+@class UIImageView;
 @class UILabel;
 @class UIButton;
-@class UIImageView;
-@class AVCaptureFileOutput;
-@class NSURL;
-@class AVCaptureConnection;
+@class UISwipeGestureRecognizer;
 @class NSString;
 @class NSBundle;
-
-SWIFT_CLASS("_TtC9ShuftiPro8CameraVc")
-@interface CameraVc : UIViewController <AVCaptureFileOutputRecordingDelegate, NSURLSessionTaskDelegate>
-@property (nonatomic, weak) IBOutlet UIView * _Null_unspecified cameraView;
-@property (nonatomic, weak) IBOutlet UIView * _Null_unspecified titleView;
-@property (nonatomic, weak) IBOutlet UIView * _Null_unspecified recordingView;
-@property (nonatomic, weak) IBOutlet UILabel * _Null_unspecified countDownLabel;
-@property (nonatomic, weak) IBOutlet UILabel * _Null_unspecified titleOnCamera;
-@property (nonatomic, weak) IBOutlet UIButton * _Null_unspecified recordButton;
-@property (nonatomic, weak) IBOutlet UIButton * _Null_unspecified backButton;
-@property (nonatomic, weak) IBOutlet UIButton * _Null_unspecified switchCameraBtn;
-@property (nonatomic, weak) IBOutlet UIButton * _Null_unspecified flashBtn;
-@property (nonatomic, weak) IBOutlet UIImageView * _Null_unspecified flashImg;
-@property (nonatomic, weak) IBOutlet UIImageView * _Null_unspecified backBtnImg;
-- (void)viewWillAppear:(BOOL)animated;
-- (void)viewDidLoad;
-- (void)viewWillDisappear:(BOOL)animated;
-- (void)viewDidDisappear:(BOOL)animated;
-- (IBAction)capture:(UIButton * _Nonnull)sender;
-- (IBAction)switchCameraBtnPressed:(id _Nonnull)sender;
-- (IBAction)backBtnPressedOnCamera:(id _Nonnull)sender;
-- (IBAction)torchSwitch:(id _Nonnull)sender;
-- (void)switchCamera;
-- (void)captureOutput:(AVCaptureFileOutput * _Nonnull)output didFinishRecordingToOutputFileAtURL:(NSURL * _Nonnull)outputFileURL fromConnections:(NSArray<AVCaptureConnection *> * _Nonnull)connections error:(NSError * _Nullable)error;
-- (void)cameraErrorMessageWithCamErrMsg:(NSString * _Nonnull)camErrMsg;
-- (void)zoomIn;
-- (void)zoomOut;
-- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
-- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
-@end
-
-@class AVCapturePhotoOutput;
-@class AVCaptureResolvedPhotoSettings;
-@class AVCaptureBracketedStillImageSettings;
-
-@interface CameraVc (SWIFT_EXTENSION(ShuftiPro)) <AVCapturePhotoCaptureDelegate>
-- (void)captureOutput:(AVCapturePhotoOutput * _Nonnull)captureOutput didFinishProcessingPhotoSampleBuffer:(CMSampleBufferRef _Nullable)photoSampleBuffer previewPhotoSampleBuffer:(CMSampleBufferRef _Nullable)previewPhotoSampleBuffer resolvedSettings:(AVCaptureResolvedPhotoSettings * _Nonnull)resolvedSettings bracketSettings:(AVCaptureBracketedStillImageSettings * _Nullable)bracketSettings error:(NSError * _Nullable)error;
-@end
-
-
-@class UISwipeGestureRecognizer;
 
 SWIFT_CLASS("_TtC9ShuftiPro9CameraVc2")
 @interface CameraVc2 : UIViewController <AVCaptureFileOutputRecordingDelegate>
@@ -347,13 +304,17 @@ SWIFT_CLASS("_TtC9ShuftiPro9CameraVc2")
 
 @class AVCaptureMetadataOutput;
 @class AVMetadataObject;
+@class AVCaptureConnection;
 
 @interface CameraVc2 (SWIFT_EXTENSION(ShuftiPro)) <AVCaptureMetadataOutputObjectsDelegate>
 - (void)captureOutput:(AVCaptureMetadataOutput * _Nonnull)captureOutput didOutputMetadataObjects:(NSArray<AVMetadataObject *> * _Nonnull)metadataObjects fromConnection:(AVCaptureConnection * _Nonnull)connection;
 @end
 
 
+@class AVCapturePhotoOutput;
 @class AVCapturePhoto;
+@class AVCaptureFileOutput;
+@class NSURL;
 
 @interface CameraVc2 (SWIFT_EXTENSION(ShuftiPro)) <AVCapturePhotoCaptureDelegate>
 - (void)captureOutput:(AVCapturePhotoOutput * _Nonnull)output didFinishProcessingPhoto:(AVCapturePhoto * _Nonnull)photo error:(NSError * _Nullable)error;
@@ -797,9 +758,12 @@ SWIFT_CLASS("_TtC9ShuftiPro13UploadVideoVc")
 
 @class UINavigationBar;
 @class UIBarButtonItem;
+@class WKWebViewConfiguration;
+@class WKNavigationAction;
+@class WKWindowFeatures;
 
 SWIFT_CLASS("_TtC9ShuftiPro17WebViewController")
-@interface WebViewController : UIViewController <WKNavigationDelegate>
+@interface WebViewController : UIViewController <WKNavigationDelegate, WKUIDelegate>
 @property (nonatomic, strong) IBOutlet UIView * _Null_unspecified containerView;
 @property (nonatomic, strong) IBOutlet UILabel * _Null_unspecified pleaseWaitLbl;
 @property (nonatomic, weak) IBOutlet UIView * _Null_unspecified loadingView;
@@ -814,13 +778,13 @@ SWIFT_CLASS("_TtC9ShuftiPro17WebViewController")
 - (void)viewDidDisappear:(BOOL)animated;
 - (void)webView:(WKWebView * _Nonnull)webView didStartProvisionalNavigation:(WKNavigation * _Null_unspecified)navigation;
 - (void)webView:(WKWebView * _Nonnull)webView didFinishNavigation:(WKNavigation * _Null_unspecified)navigation;
+- (WKWebView * _Nullable)webView:(WKWebView * _Nonnull)webView createWebViewWithConfiguration:(WKWebViewConfiguration * _Nonnull)configuration forNavigationAction:(WKNavigationAction * _Nonnull)navigationAction windowFeatures:(WKWindowFeatures * _Nonnull)windowFeatures SWIFT_WARN_UNUSED_RESULT;
 - (void)webView:(WKWebView * _Nonnull)webView didFailNavigation:(WKNavigation * _Null_unspecified)navigation withError:(NSError * _Nonnull)error;
 - (IBAction)navBarCancelBtnPressed:(id _Nonnull)sender;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 @end
 
-@class WKNavigationAction;
 
 @interface WebViewController (SWIFT_EXTENSION(ShuftiPro))
 /// When a navigation action occurs for a link to wikipedia, ensure it gets
